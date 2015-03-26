7db84f1a7c1ba23dbb355229e186fd1d7c071c4d
グミ <hi@gumi.nu>
2015-03-25 23:38:07 -0700

    play the stream

--- app/src/main/java/nu/gumi/radio/MainActivity.java
+++ app/src/main/java/nu/gumi/radio/MainActivity.java
@@ -1,17 +1,30 @@
 package nu.gumi.radio;
 
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements SurfaceHolder.Callback, MediaPlayer.OnPreparedListener {
+
+    private MediaPlayer mediaPlayer;
+    private SurfaceHolder musicHolder;
+    private SurfaceView musicSurface;
+    String vidAddress = "https://stream.r-a-d.io/main.mp3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        musicSurface = (SurfaceView) findViewById(R.id.surfView);
+        musicHolder = musicSurface.getHolder();
+        musicHolder.addCallback(this);
     }
 
 
@@ -36,4 +49,37 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder arg0) {
+        // Setup
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDisplay(musicHolder);
+            mediaPlayer.setDataSource(vidAddress);
+            mediaPlayer.prepare();
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        // Start playback
+        mediaPlayer.start();
+    }
+
 }
